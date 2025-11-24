8d52bcc989aa67e529d7236e087cf5728df14eeb
varshini071 <varshinivangala703@gmail.com>
2025-11-24 19:47:27 +0530

    sumnum.c

---
+++ sumnum.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+     int num1=10;
+    int num2=20;
+    int num3=30;
+    int num4=40;    
+    int num5=50;
+    int sum = num1 + num2 + num2 + num3 + num1 + num4 + num5 +num3+num5;
+    printf("Sum = %d\n", sum);
+    return 0;
+}
+    
